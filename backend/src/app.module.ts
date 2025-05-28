f70f32cfa63e8d01ccc00cee64ba72916c6f1560
mohammed595 <Mohammed9Ne@gmail.com>
2025-05-28 21:20:56 +0300

    feat: Integrate iTunes search module into app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SearchHistory } from './itunes-search/entities/search-history.entity';
+import { SearchResult } from './itunes-search/entities/search-result.entity';
+import { ItunesSearchModule } from './itunes-search/itunes-search.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_DATABASE || 'postgres',
+      entities: [SearchResult, SearchHistory],
+      synchronize: true,
+    }),
+    ItunesSearchModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
