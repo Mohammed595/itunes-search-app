bd8979efce6a81a73cb2e3e1c6957edfbc3725de
mohammed595 <Mohammed9Ne@gmail.com>
2025-05-28 21:21:07 +0300

    config: Update port configuration and add CORS support

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   await app.listen(3002);
+  console.log('Application is running on: http://localhost:3002');
 }
 bootstrap();
